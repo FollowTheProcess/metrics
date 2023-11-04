6f6e545a1acf8da9a26501fd850f8487deaa6198
Tom Fleet <tom.fleet@chetwood.co>
2023-11-04 13:53:00 +0000

    Implement the metadata data model

--- emf.go
+++ emf.go
@@ -1,7 +1,49 @@
 // Package emf is a placeholder for something cool.
 package emf
 
-// Hello returns a welcome message for the project.
-func Hello() string {
-	return "Hello emf"
+// StorageResolution represents the metrics resolution.
+type StorageResolution int
+
+const (
+	HighResolution     StorageResolution = 1  // 1 second resolution, for high precision metrics
+	StandardResolution StorageResolution = 60 // Minute resolution, suitable for most metrics
+)
+
+// Metadata encodes the EMF Metadata object.
+type Metadata struct {
+	// Optional name of the CloudWatch log group.
+	LogGroupName string `json:"LogGroupName,omitempty"`
+
+	// List of metric directives.
+	Metrics []Metric `json:"CloudWatchMetrics"`
+
+	// UNIX (milliseconds) timestamp for the metric.
+	Timestamp int64 `json:"Timestamp"`
+}
+
+// Metric encodes the EMF MetricDirective object.
+type Metric struct {
+	// The CloudWatch namespace for the metric.
+	Namespace string `json:"Namespace"`
+
+	// List of EMF dimension keys.
+	Dimensions []Dimension `json:"Dimensions"`
+
+	// The actual metric definitions.
+	Metrics []MetricDefinition `json:"Metrics"`
+}
+
+// Dimension encodes a single EMF metric dimension.
+type Dimension []string
+
+// MetricDefinition encodes a single EMF metric definition.
+type MetricDefinition struct {
+	// The name of the metric
+	Name string `json:"Name"`
+
+	// The unit of measurement, optional. If omitted, None is assumed
+	Unit Unit `json:"Unit,omitempty"`
+
+	// Resolution for the metric, optional. If omitted, standard resolution is assumed.
+	Resolution StorageResolution `json:"StorageResolution,omitempty"`
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/FollowTheProcess/emf
 
 go 1.21
+
+require (
+	github.com/FollowTheProcess/test v0.6.0
+	github.com/kinbiko/jsonassert v1.1.1
+)
+
+require github.com/google/go-cmp v0.6.0 // indirect
